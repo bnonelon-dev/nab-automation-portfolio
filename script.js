9e38ac61c17adef5ad2b76a54468f501b815e03c
Nonelon Alcazar Bueno <bnonelon@gmail.com>
2026-07-13 21:37:34 +0800

    Update portfolio design

--- script.js
+++ script.js
@@ -3,23 +3,12 @@
 const reveals=document.querySelectorAll('.reveal');
 
 function reveal(){
-
-reveals.forEach((item)=>{
-
-const top=item.getBoundingClientRect().top;
-
-if(top<window.innerHeight-120){
-
-item.classList.add("active");
-
+  reveals.forEach((item)=>{
+    item.classList.add("active");
+  });
 }
 
-})
-
-}
-
-window.addEventListener("scroll",reveal);
-
+window.addEventListener("scroll", reveal);
 reveal();
 
 
